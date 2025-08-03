40c7bddf93fc4c1df0e81b1e638c56f9448a6e8b
Kiran-Pro <krc5win@gmail.com>
2025-08-03 21:00:49 +0530

    Text change

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -41,7 +41,7 @@ const LoginPage = () => {
     <div className="min-h-screen flex items-center justify-center bg-blue-50">
       <div className="bg-white p-8 rounded-2xl shadow-md w-full max-w-md border border-blue-100">
         <h2 className="text-3xl font-bold text-blue-700 mb-6 text-center">
-          BulkMail Login
+          BulkMail
         </h2>
 
         <form onSubmit={handleLogin} className="space-y-5">
